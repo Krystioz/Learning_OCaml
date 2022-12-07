24f21ebf2a9bd45dd1c3f60512102a72d702c12c
Krystian Głuszko <gluszkokrystian@gmail.com>
2022-12-07 22:37:59 +0100

    fib and fast fib funcs implementation

--- _build/default/ex_1.ml
+++ _build/default/ex_1.ml
@@ -15,21 +15,9 @@ print_endline @@  string_of_int @@ -1;; *)
 
 let rms x y = square x +. square y |> (fun x -> x /. 2.) |> sqrt;; *)
 
-(* let valid_date d m = match m with 
+let valid_date d m = match m with 
 | "feb" -> d <= 28 && d >= 1 
 | _ -> false
 ;;
 
-valid_date (-1) "feb" |> string_of_bool |> print_endline;; *)
-
-
-
-let rec fib x  = if x = 2 || x = 1 then 1 else 
-        fib (x-1) + fib(x-2)
-;;
-
-
-let rec helper n pp p =
-  if n = 1 then p
-  else helper (n-1) p (pp+p)
-let fib_fast n = helper n 0 1
+valid_date (-1) "feb" |> string_of_bool |> print_endline;;
